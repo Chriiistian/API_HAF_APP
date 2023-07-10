079a129a370b33c5318dbc3c46477ba04fab737f
Chriiistian <christian.hernandez.aranda@alumnos.uta.cl>
2023-07-09 20:48:19 -0400

    update-shop-history-ch

--- api/api.py
+++ api/api.py
@@ -12,16 +12,28 @@ conn = psycopg2.connect(
     port="5433"
 )
 
-# Ruta para obtener información de los usuarios
-@app.route('/users', methods=['GET'])
-def obtener_usuarios():
-    # Lógica para obtener los usuarios de la base de datos
-    cur = conn.cursor()
-    cur.execute("SELECT u_name, email FROM users")
-    usuarios = cur.fetchall()
+# Ruta para el inicio de sesión
+@app.route('/login', methods=['POST'])
+def login():
+    # Obtiene los datos del inicio de sesión desde la solicitud
+    datos_login = request.get_json()
 
-    # Retorna los usuarios en formato JSON
-    return jsonify(usuarios)
+    # Extraer los datos del inicio de sesión
+    email = datos_login['email']
+    passw = datos_login['password']
+
+    # Verificar las credenciales de inicio de sesión en la base de datos
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE email = %s AND passw = %s", (email, passw))
+    user = cur.fetchone()
+
+    if user is not None:
+        # Las credenciales de inicio de sesión son correctas
+        return jsonify({'mensaje': 'Inicio de sesión exitoso'})
+    else:
+        # Las credenciales de inicio de sesión son incorrectas
+        return jsonify({'mensaje': 'Credenciales de inicio de sesión incorrectas'}), 401
+
 
 # Ruta para obtener información de los ítems
 @app.route('/items', methods=['GET'])
@@ -36,60 +48,39 @@ def obtener_items():
     for item in items:
         item_dict = {
             'id': item[0],
-            'nombre': item[1],
-            'precio': item[2],
-            'descripcion': item[3]
+            'name': item[1],
+            'price': item[2],
+            'descripcion': item[3],
+            'qty': item[4],
+            'category': item[5]
         }
         lista_items.append(item_dict)
 
     # Retorna los ítems en formato JSON
     return jsonify(lista_items)
 
-# Ruta para autenticar usuarios
-@app.route('/login', methods=['POST'])
-def iniciar_sesion():
-    # Obtiene los datos de inicio de sesión desde la solicitud
-    datos_sesion = request.get_json()
-
-    # Lógica para autenticar al usuario
-    usuario = datos_sesion['usuario']
-    contraseña = datos_sesion['contraseña']
-
-    # Verificar las credenciales del usuario en la base de datos
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE u_name = %s AND pass = %s", (usuario, contraseña))
-    resultado = cur.fetchone()
-
-    if resultado:
-        # Autenticación exitosa
-        return jsonify({'mensaje': 'Inicio de sesión exitoso'})
-    else:
-        # Autenticación fallida
-        return jsonify({'mensaje': 'Error al iniciar sesión'})
-
-# Ruta para crear una orden de compra
+# Ruta para crear órdenes de compra
 @app.route('/ordenes', methods=['POST'])
-def crear_orden_compra():
-    # Obtiene los datos de la orden desde la solicitud
-    datos_orden = request.get_json()
+def crear_ordenes_compra():
+    # Obtiene los datos de las órdenes desde la solicitud
+    datos_ordenes = request.get_json()
 
-    # Extraer los datos de la orden
-    id_user = datos_orden['id_usuario']
-    id_item = datos_orden['id_item']
-    shop_date = datos_orden['fecha']
-    value = datos_orden['valor']
-
-    # Insertar la orden de compra en la base de datos
-    cur = conn.cursor()
-    cur.execute("INSERT INTO shop (id_user, id_item, shop_date, value) VALUES (%s, %s, %s, %s)",
-                (id_user, id_item, shop_date, value))
-    conn.commit()
+    for datos_orden in datos_ordenes:
+        # Extraer los datos de cada orden
+        value = datos_orden['valor']
+        shop_date = datos_orden['fecha']
+        id_item = datos_orden['id_item']
+        id_user = datos_orden['id_user']
+        
+        # Insertar cada orden de compra en la base de datos
+        cur = conn.cursor()
+        cur.execute("INSERT INTO shop (id_user, id_item, shop_date, value) VALUES (%s, %s, %s, %s)",
+                    (id_user, id_item, shop_date, value))
+        conn.commit()
 
     # Retorna una respuesta de éxito en formato JSON
-    return jsonify({'mensaje': 'Orden de compra creada correctamente'})
+    return jsonify({'mensaje': 'Órdenes de compra creadas correctamente'})
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
+    app.run(debug=True, port=8000)
